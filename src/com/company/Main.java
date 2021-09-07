01048d6796b8930c884f254d160e3ea52dfd6d4c
marc572m <marc572m@edu.easj.dk>
2021-09-07 12:41:04 +0200

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+
+        int x = 10;
+        double y = 10;
+
+        System.out.printf("%f", x);
+        System.out.printf("%d", y);
+        System.out.printf("%d %f", y);
+    }
+}
